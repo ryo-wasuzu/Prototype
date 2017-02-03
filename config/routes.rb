2855b54b13fbebb412ac6a059de91abea777351f
ryo-wasuzu <ryo1110130336@icloud.com>
2017-02-03 15:38:44 +0900

    routing 作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :ideas do
+  resources :comments,only: [:create]
+end
+  resources :users, only: [:show]
   root 'ideas#index'
 end
